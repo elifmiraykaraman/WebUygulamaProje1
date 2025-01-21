b59682dc8cd56527f2c893f6478da8dcbb0eb869
ElifMiray <elifmiraykaraman@icloud.com>
2025-01-21 19:44:59 +0300

    Initial commit of WebUygulamaProje1

---
+++ Models/KitapTuru.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;    
+namespace WebUygulamaProje1.Models
+{
+    public class KitapTuru
+    {
+        [Key] //primary key
+        public int Id { get; set; }
+        [Required] //not null
+        public string Ad { get; set; }  
+    }
+}
---
+++ Utility/UygulamaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebUygulamaProje1.Utility
+{
+    public class UygulamaDbContext : DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options) { }
+
+        public DbSet<WebUygulamaProje1.Models.KitapTuru> KitapTurleri { get; set; }
+
+    }
+}
