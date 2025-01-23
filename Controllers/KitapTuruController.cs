ed7c8fbec802b90a46d9ac7de1a5355ca8c1b857
ElifMiray <elifmiraykaraman@icloud.com>
2025-01-23 14:21:05 +0300

    ilk MVC mimarisi yapısına ulaşıldı. kitap türleri sql den çekildi

---
+++ Controllers/KitapTuruController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebUygulamaProje1.Utility;
+using WebUygulamaProje1.Models;
+
+namespace WebUygulamaProje1.Controllers
+{
+    public class KitapTuruController : Controller
+    {
+        private readonly UygulamaDbContext _uygulamaDbContext;
+        public KitapTuruController(UygulamaDbContext context)
+        {
+            _uygulamaDbContext = context;
+        }
+        public IActionResult Index()
+        {
+            List<KitapTuru> objKitapTuruList  = _uygulamaDbContext.KitapTurleri.ToList();
+            return View(objKitapTuruList);
+        }
+    }
+}
