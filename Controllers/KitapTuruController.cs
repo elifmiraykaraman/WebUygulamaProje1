d94036e917b6c77d288df59ef92ce3ccfb08f9f0
ElifMiray <elifmiraykaraman@icloud.com>
2025-01-24 16:01:44 +0300

    yeni kitap türü ekleyip veri tabanına yazdı.doğrulama işlemleri eklendi.Güncelle ve Sil butonları eklendi.Butonların Backend leri yazıldı.

--- Controllers/KitapTuruController.cs
+++ Controllers/KitapTuruController.cs
@@ -13,14 +13,53 @@ namespace WebUygulamaProje1.Controllers
         }
         public IActionResult Index()
         {
-            List<KitapTuru> objKitapTuruList  = _uygulamaDbContext.KitapTurleri.ToList();
+            List<KitapTuru> objKitapTuruList = _uygulamaDbContext.KitapTurleri.ToList();
             return View(objKitapTuruList);
         }
 
         public IActionResult Ekle()
         {
-            return View();  
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Ekle(KitapTuru kitapTuru)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _uygulamaDbContext.KitapTurleri.Add(kitapTuru);
+                _uygulamaDbContext.SaveChanges();  //SaveChanges yapmazsanız bilgiler veri tabanına eklenmez!
+                return RedirectToAction("Index", "KitapTuru");
+            }
+            return View();
+
         }
 
+    public IActionResult Guncelle(int? id)
+        {
+            if (id == null || id == 0) 
+            { 
+            return NotFound();
+            }
+            KitapTuru? kitapTuruVt = _uygulamaDbContext.KitapTurleri.Find(id);
+            if(kitapTuruVt == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Guncelle(KitapTuru kitapTuru)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _uygulamaDbContext.KitapTurleri.Update(kitapTuru);
+                _uygulamaDbContext.SaveChanges();  //SaveChanges yapmazsanız bilgiler veri tabanına eklenmez!
+                return RedirectToAction("Index", "KitapTuru");
+            }
+            return View();
+
+        }
     }
 }
--- Models/KitapTuru.cs
+++ Models/KitapTuru.cs
@@ -1,11 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;    
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;    
 namespace WebUygulamaProje1.Models
 {
     public class KitapTuru
     {
         [Key] //primary key
         public int Id { get; set; }
-        [Required] //not null
+        [Required(ErrorMessage ="Kitap Türü Adı Boş Bırakılamaz!")] //not null
+        [MaxLength(25)]
+        [DisplayName("Kitap Türü Adı")]
         public string Ad { get; set; }  
     }
 }
