da7f8e7b70b784dc504b0a8f4c0f370118e9c53e
ElifMiray <elifmiraykaraman@icloud.com>
2025-01-26 14:12:50 +0300

    Kiralama Controller Katmanı Sınıfı Oluşturuldu.

---
+++ Controllers/KiralamaController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebUygulamaProje1.Utility;
+using WebUygulamaProje1.Models;
+
+namespace WebUygulamaProje1.Controllers
+{
+    public class KiralamaController : Controller
+    {
+        private readonly IKiralamaRepository _kiralamaRepository;
+        private readonly IKitapRepository _kitapRepository;
+        public readonly IWebHostEnvironment _webHostEnviroment;
+
+        public KiralamaController(IKiralamaRepository kiralamaRepository, IKitapRepository kitapRepository, IWebHostEnvironment webHostEnviroment)
+        {
+            _kiralamaRepository = kiralamaRepository;
+            _kitapRepository = kitapRepository;
+            _webHostEnviroment = webHostEnviroment;
+        }
+        public IActionResult Index()
+        {
+            List<Kiralama> objKiralamaList = _kiralamaRepository.GetAll(includeProps:"Kitap").ToList();
+            return View(objKiralamaList);
+        }
+
+        public IActionResult EkleGuncelle(int? id)
+        {
+            IEnumerable<SelectListItem> KitapList = _kitapRepository.GetAll().Select(k => new SelectListItem
+            {
+                Text = k.KitapAdi,
+                Value = k.Id.ToString()
+            });
+
+            ViewBag.KitapList = KitapList;
+            if(id== null || id == 0)
+            {
+                //ekle
+                return View();
+            }
+     
+            else
+            {
+                //guncelleme
+                Kiralama? kiralamaVt = _kiralamaRepository.Get(u => u.Id == id);  //Expression<Func<T, bool>> filtre
+                if (kiralamaVt == null)
+                {
+                    return NotFound();
+                }
+                return View(kiralamaVt);
+            }
+        }
+        
+        [HttpPost]
+        public IActionResult EkleGuncelle(Kiralama kiralama)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (kiralama.Id == 0)
+                {
+                    _kiralamaRepository.Ekle(kiralama);
+                    TempData["basarili"] = "Yeni Kiralama Kaydı Başarıyla Oluşturuldu!";
+
+                }
+                else
+                {
+                    _kiralamaRepository.Guncelle(kiralama);
+                    TempData["basarili"] = "Kiralama Kayıt  Guncelleme Başarılı !";
+
+                }
+
+                _kiralamaRepository.Kaydet();  //SaveChanges yapmazsanız bilgiler veri tabanına eklenmez!
+                return RedirectToAction("Index", "Kiralama");
+            }
+                return View();
+            
+        }
+
+
+        //GET ACTİON
+        public IActionResult Sil (int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Kiralama? kiralamaVt = _kiralamaRepository.Get(u => u.Id == id);
+            if (kiralamaVt == null)
+            {
+                return NotFound();
+            }
+            return View(kiralamaVt);
+        }
+
+        //POST ACTION
+        [HttpPost, ActionName("Sil")]
+        public IActionResult SilPOST(int? id)
+        {
+
+           Kiralama? kiralama = _kiralamaRepository.Get(u => u.Id == id);
+            if (kiralama == null)
+            {
+                return NotFound();
+            }
+            _kiralamaRepository.Sil(kiralama);
+            _kiralamaRepository.Kaydet();
+            TempData["basarili"] = "Kayıt Silme İşlemi Başarılı!";
+            return RedirectToAction("Index", "Kiralama");
+
+        }
+    }
+}
