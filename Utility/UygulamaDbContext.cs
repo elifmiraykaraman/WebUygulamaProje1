8191c0af40606509bb4ddc7d435052353beab950
ElifMiray <elifmiraykaraman@icloud.com>
2025-01-26 13:38:19 +0300

    Kiralamalar yapısı oluşturuldu.

--- Utility/UygulamaDbContext.cs
+++ Utility/UygulamaDbContext.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebUygulamaProje1.Models;
 
+//Veri tabanında EF Tablos oluşturması için ilgili model yapılarını buraya ekleriz.
 namespace WebUygulamaProje1.Utility
 {
     public class UygulamaDbContext : DbContext
     {
         public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options) { }
 
-        public DbSet<WebUygulamaProje1.Models.KitapTuru> KitapTurleri { get; set; }
-        public DbSet<WebUygulamaProje1.Models.Kitap> Kitaplar { get; set; }
+        public DbSet<KitapTuru> KitapTurleri { get; set; }
+        public DbSet<Kitap> Kitaplar { get; set; }
+
+        public DbSet<Kiralama> Kiralamalar { get; set; }
 
     }
 }
